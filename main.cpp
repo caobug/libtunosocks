ecb643d82ac3ee08e9fced20cfe5ea3a03a6a795
mario <liuminghao233@gmail.com>
2019-01-02 15:06:50 +0800

    ..

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
 #include "net/socksifier.h"
 #include "utils/logger.h"
-#include "tuntap/tuntapinstaller.h"
 
 
 int main()
 {
 	Logger::GetInstance()->InitLog();
     Logger::GetInstance()->SetLevel(spdlog::level::debug);
-	//TuntapInstaller::GetInstance()->Uninstall();
-	if (!TuntapInstaller::GetInstance()->Find())
-	{
-		TuntapInstaller::GetInstance()->Install();
-	}
+
 
     auto res = Socksifier::GetInstance()->Init();
 
     if (res)
     {
         Socksifier::GetInstance()->AsyncRun();
-
-//        sleep(3);
-////
-//        Socksifier::GetInstance()->Stop();
-////
-//        sleep(3);
-////
-//        Socksifier::GetInstance()->Restart();
-
     }
 
     getchar();
